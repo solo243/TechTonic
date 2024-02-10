e0ac7fa1235d5cd3738135152778dfb15d62276c
harshil <welcomesir48u@gmail.com>
2024-02-11 00:20:09 +0530

    init

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -5,16 +5,16 @@ function Aboutus() {
         <section className='w-full  flex'>
             <div className='flex md:flex-row flex-col '>
                 <div className='md:py-10 py-1 px-10 flex flex-col justify-center w-full '>
-                    <div className='text-primary md:text-3xl text-[6vw]'>
+                    <div className='font-semibold text-primary md:text-3xl text-[6vw]'>
                         We are Devlopers
                     </div>
-                    <div className='text-white md:text-6xl text-[8vw] leading-none tracking-tight'>
+                    <div className=' font-semibold text-white md:text-[3.7vw]  text-[8vw]  tracking-tight'>
                         We can develope per
                     </div>
-                    <div className='text-white md:text-6xl text-[8vw] mb-6'>
+                    <div className='font-semibold leading-[2vw] text-white md:text-[3.7vw] text-[8vw] mb-8'>
                         Your Requirement
                     </div>
-                    <div className=' text-gray-500 md:text-2xl text-1xl md:w-[40vw]  w-[80vw]'>
+                    <div className=' text-gray-500 md:text-[1.3vw] text-1xl md:w-[40vw]  w-[80vw]'>
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                         Distinctio, non sequi! Culpa quos, impedit, dignissimos, nam consequuntur placeat iusto excepturi quisquam deleniti
                         eligendi modi maiores id aperiam amet a eius.
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,18 +12,17 @@ function Header() {
                  flex md:flex-row flex-col justify-between 
                  overflow-hidden '>
                     <div>
-
                         <div className='text-yellow-300 mb-3 font-semibold'>
                             Hi, we are TechTonic
                         </div>
-                        <h1 className='flex  md:text-7xl text-white text-4xl'>
+                        <h1 className='flex font-semibold md:text-[5vw] text-white text-4xl'>
                             TechTonic
                             Studio
                         </h1>
-                        <h1 className='md:mt-3  md:text-6xl text-white text-4xl'>
-                            Innovative <span className='text-[#9270ff]'>Excellence </span>
+                        <h1 className='md:mt-[2.6vw] font-semibold md:text-[5vw] text-white text-4xl'>
+                            Innovative <span className='text-[#9270ff] font-bold'>Excellence </span>
                         </h1>
-                        <button className='bg-primary font-semibold py-3  md:px-10 px-10 rounded-full md:mt-[3.7vw] mt-9 text-white'>
+                        <button className='bg-primary  font-bold py-3  md:px-10 px-10 rounded-full md:mt-[3.7vw] mt-9 text-white'>
                             Contect us
                         </button>
                     </div>
