f71c687c464cc5d8fdbf061c1389536ab753e892
harshil <welcomesir48u@gmail.com>
2024-02-12 17:04:38 +0530

    init

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Hero from '../assets/About.png'
+import { motion } from 'framer-motion'
 function Aboutus() {
     return (
+
         <section id="about" className='w-full  flex'>
             <div className='flex md:flex-row flex-col '>
-                <div data-scroll  className='md:py-10 py-1 px-10 flex flex-col justify-center w-full '>
+                <motion.div 
+                    className='md:py-10 py-1 px-10 flex flex-col justify-center w-full '>
                     <div className='font-semibold text-primary md:text-3xl text-[6vw]'>
                         We are Devlopers
                     </div>
@@ -17,7 +20,7 @@ function Aboutus() {
                     <div className=' text-gray-500 md:text-[1.3vw] text-1xl md:w-[40vw]  w-[80vw]'>
                         Welcome to TechTonic, where we turn your vision into reality. Our dedicated team specializes in crafting bespoke development solutions tailored to your needs. With a focus on excellence and innovation, we exceed expectations at every turn. Let us empower your business to thrive in today's dynamic landscape. Experience the difference with TechTonic.
                     </div>
-                </div>
+                </motion.div>
                 <img src={Hero} className='md:w-[60vw] lg:flex w-[100vw] px-8 md:h-[40vw] h-[80vw] ' />
 
             </div>
